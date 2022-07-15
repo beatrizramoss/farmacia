2450f4991118ccc9275ea79877907edba230dece
Beatriz <beatriizramoss@gmail.com>
2022-07-14 23:36:10 -0300

    Feat: CRUD farmacia

---
+++ farmacia/src/main/java/br/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,74 @@
+package br.com.generation.farmacia.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size (min=5, max=100)
+	private String nomeDoProduto;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	private int estoque;
+	
+	private Date validade;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeDoProduto() {
+		return nomeDoProduto;
+	}
+
+	public void setNomeDoProduto(String nomeDoProduto) {
+		this.nomeDoProduto = nomeDoProduto;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+	public Date getValidade() {
+		return validade;
+	}
+
+	public void setValidade(Date validade) {
+		this.validade = validade;
+	}
+
+}
